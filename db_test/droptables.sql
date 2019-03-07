a6a6920d8a46dac8d9c8ab7b2bebf5d7c439f7a6
Johan Guldmyr <johan.guldmyr@csc.fi>
2019-03-07 14:38:57 +0000

    How to get to Ocata

---
+++ db_test/droptables.sql
@@ -0,0 +1,11 @@
+drop database barbican;
+drop database cinder;
+drop database glance;
+drop database heat;
+drop database keystone;
+drop database magnum;
+drop database neutron;
+drop database nova;
+drop database nova_api;
+drop database nova_cell0;
+
